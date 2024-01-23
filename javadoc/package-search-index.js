2af33731cf5c93e66953111dbe31e25d1a160207
Iulian <54375582+poenaruiulian@users.noreply.github.com>
2024-01-23 16:10:04 +0200

    Delete javadoc directory

--- javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main"},{"l":"main.java.constants"},{"l":"main.java.data"},{"l":"main.java.helpers"},{"l":"main.java.types"}];updateSearchResults();
